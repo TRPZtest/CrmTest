32e1b9c8308ca40160e118e3ed9bceb49d7fc8b8
Dmytro Vyshnvskyi <vishnevsky.dmtr@gmail.com>
2024-06-12 01:09:31 +0300

    User id as int

--- CrmTest/Data/ApplicationDbContext.cs
+++ CrmTest/Data/ApplicationDbContext.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CrmTest.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser<int>, IdentityRole<int>, int>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+   
         }
     }
 }
