a74d4b7d244625e4c8cc8ca3b5a08d204b341c5e
Dmytro Vyshnvskyi <vishnevsky.dmtr@gmail.com>
2024-06-11 21:58:04 +0300

    Add project files.

---
+++ CrmTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CrmTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
